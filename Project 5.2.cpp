e31ea5ee60cccc8471dbe57783593e8ff73db323
Nazar Kuziv <nazar123kuziv@gmail.com>
2020-11-21 16:00:20 +0200

    first commit

---
+++ Project 5.2.cpp
@@ -0,0 +1,69 @@
+﻿// Lab_05_2.cpp
+// < Кузів Назар >
+// Лабораторна робота № 5.2
+// Функції, що містять арифметичний вираз
+// Варіант 6
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+void S(const double x, const double eps, int &n, double & s);
+void A(const double x, const int n, double &a);
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	double xp, xk, x, dx, eps, s = 0;
+	int n;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(28) << "Ряд Тейлора" << setw(15) << "|" << endl;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(9) << "exp(x)" << "  |"
+		<< setw(7) << "s" << "    |"
+		<< setw(5) << "n" << "    |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+
+	x = xp;
+	while (x <= xk && x >= -1 && x < 1)
+	{
+		S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(4) << exp(x) << " |"
+			<< setw(10) << setprecision(4) << s << " |"
+			<< setw(5) << n << "    |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+
+void S(const double x, const double eps, int &n, double &s)
+{
+	n = 1; 
+	double a = x; 
+	s = a;
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+		s *= -1;
+	} while (a >= eps);
+}
+
+void A(const double x, const int n, double &a)
+{
+	double R = x * (n - 1.) / n; 
+	a *= R;
+}
